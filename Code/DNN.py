151c6b229f4104752d63b6a42540d2431db921bf
Between Hope and Risk <somayah.albaradei@kaust.edu.sa>
2021-07-20 03:15:25 +0300

    Update DNN.py

--- Code/DNN.py
+++ Code/DNN.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# after completing the CVAE training process, 
+# the encoder part is fed to a DNN classifier.
+# We used the latent vector as input to train the classifier to make prediction.
 
 
 from keras.models import Sequential
@@ -10,14 +12,22 @@ from tensorflow.keras.callbacks import EarlyStopping
 early_stopping = EarlyStopping(patience=3, monitor='val_loss')
 
 # create model
+# Building the DNN requires configuring the layers of the model, then compiling the model. 
+# First we stack a few layers together using. 
+# Next we configure the loss function, optimizer, and metrics to monitor.
+
 model = Sequential()
 model.add(Dense(60, input_dim=100, activation='tanh'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.3))
 model.add(Dense(30, activation='tanh', kernel_regularizer=l2(0.01), bias_regularizer=l2(0.01)))
-model.add(Dropout(0.5))
+model.add(Dropout(0.3))
 model.add(Dense(1, activation='sigmoid'))
 # Compile model
 model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
+
+#Training the DNN model 
+
+#Feed the training data to the model, the model learns to associate features and labels.
 
 history =model.fit(x_train, y_train,
         epochs=100,
